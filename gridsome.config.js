7655b06c3b994779d60bfc962011b4b1ede79371
Christian Stoyanov <cristian.stoyanov@gmail.com>
2020-03-21 20:44:44 +0100

    Working on making this blog. Added plugins and a first post. Following https://github.com/gridsome/gridsome-starter-blog

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,11 +5,41 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
+  siteName: 'Crissto\'s blog',
   siteUrl: 'https://blog.crissto.com',
+
+  templates: {
+    Post: '/:title',
+    Tag: '/tag/:id'
+  },
+
   plugins: [
     {
       use: 'gridsome-plugin-tailwindcss'
+    },
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        typeName: 'Post',
+        path: 'content/posts/*.md',
+        refs: {
+          tags: {
+            typeName: 'Tag',
+            create: true
+          }
+        }
+      }
     }
-  ]
+  ],
+
+  transformers: {
+    remark: {
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+      anchorClassName: 'icon icon-link',
+      plugins: [
+        '@gridsome/remark-prismjs'
+      ]
+    }
+  }
 }
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,29 +1,51 @@
 <template>
   <Layout>
-
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
     <g-image alt="Example image" src="~/favicon.png" width="135" />
 
     <h1>Hello, world! This is Crissto's Blog</h1>
 
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores</p>
 
     <p class="home-links">
       <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
       <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
     </p>
 
+    <div v-for="edge in $page.posts.edges" :key="edge.node.id">
+      {{ edge.node }}
+    </div>
   </Layout>
 </template>
+
+<page-query>
+query {
+  posts: allPost(filter: { published: { eq: true }}) {
+    edges {
+      node {
+        id
+        title
+        date (format: "D. MMMM YYYY")
+        timeToRead
+        description
+        path
+        tags {
+          id
+          title
+          path
+        }
+      }
+    }
+  }
+}
+</page-query>
 
 <script>
 export default {
   metaInfo: {
-    title: 'Blog'
+    title: "Blog"
   }
-}
+};
 </script>
 
 <style>
